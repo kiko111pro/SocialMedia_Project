079e2b91412e14139a826f29e339eef56efd6d1b
Kshitiz Pal <65704651+kiko111pro@users.noreply.github.com>
2021-08-08 18:15:11 +0530

    Update setup.js

--- setup.js
+++ setup.js
@@ -3,15 +3,15 @@ import firebase from '@react-native-firebase/app';
 import auth from '@react-native-firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCbkvCkRcQ5azlm-pe36bdKQq6EcbhOm1I',
-  authDomain: 'social-project-5911c.firebaseapp.com',
-  projectId: 'social-project-5911c',
-  storageBucket: 'social-project-5911c.appspot.com',
-  messagingSenderId: '44186900827',
-  appId: '1:44186900827:web:e6dcd291709dad6dfbe27e',
-  measurementId: 'G-H8VDZEN9KW',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
   databaseURL:
-    'https://social-project-5911c-default-rtdb.asia-southeast1.firebasedatabase.app',
+    '',
 };
 
 if (!firebase.apps.length) {
